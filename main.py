1deac5debaf4b47b868b101d9dcf9eb4fbd931aa
Tasnim Ali <rajonali44492@gmail.com>
2017-07-17 23:16:08 -0500

    Delete material folder contents at start

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def allowed_file(filename):
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     folder = 'static/file_queue/'
+    folder2 = 'material/'
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
         try:
@@ -40,7 +41,16 @@ def home():
             #elif os.path.isdir(file_path): shutil.rmtree(file_path)
         except Exception as e:
             print(e)
+    for the_file in os.listdir(folder2):
+        file_path = os.path.join(folder2, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
     return render_template('index.html')
+
 
 
 
